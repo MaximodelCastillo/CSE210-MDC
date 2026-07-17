ecee322081d8542c484a9140639db1f682403839
Maximo del Castillo <111653854+MaximodelCastillo@users.noreply.github.com>
2026-07-17 11:41:35 -0300

    Implement Scripture class for memorization functionality

---
+++ week03/ScriptureMemorizer/scripture.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        _random = new Random();
+
+        string[] parts = text.Split(' ');
+
+        foreach (string part in parts)
+        {
+            _words.Add(new Word(part));
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        int hidden = 0;
+
+        while (hidden < numberToHide && !IsCompletelyHidden())
+        {
+            int index = _random.Next(_words.Count);
+
+            if (!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                hidden++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            text += word.GetDisplayText() + " ";
+        }
+
+        return text;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

680211f5d450d4f969fbe2190ce999334589418b
Maximo del Castillo <111653854+MaximodelCastillo@users.noreply.github.com>
2026-07-27 17:38:14 -0300

    Refactor Product class structure and methods

--- week04/OnlineOrdering/product.cs
+++ week04/OnlineOrdering/product.cs
@@ -1,32 +1,35 @@
 using System;
 
-public class Product
+namespace OnlineOrdering
 {
-    private string _name;
-    private string _productId;
-    private double _price;
-    private int _quantity;
-
-    public Product(string name, string productId, double price, int quantity)
+    public class Product
     {
-        _name = name;
-        _productId = productId;
-        _price = price;
-        _quantity = quantity;
-    }
+        private string _name;
+        private string _productId;
+        private double _price;
+        private int _quantity;
 
-    public string GetName()
-    {
-        return _name;
-    }
+        public Product(string name, string productId, double price, int quantity)
+        {
+            _name = name;
+            _productId = productId;
+            _price = price;
+            _quantity = quantity;
+        }
 
-    public string GetProductId()
-    {
-        return _productId;
-    }
+        public string GetName()
+        {
+            return _name;
+        }
 
-    public double GetTotalCost()
-    {
-        return _price * _quantity;
+        public string GetProductId()
+        {
+            return _productId;
+        }
+
+        public double GetTotalCost()
+        {
+            return _price * _quantity;
+        }
     }
 }
